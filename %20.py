fa6f73db473be62292a2159d4d9deda4bbc3205f
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-15 19:02:16 +0300

    Added Comment line

--- %20.py
+++ %20.py
@@ -1,3 +1,5 @@
+# Ortalamadan %20 den daha uzak olanları seçen bir liste üretici yaz
+
 x=[0,1,2,3,4,5,6,7,8,9,10]
 yuzde=20
 y=[i for i in x if(i>((yuzde+100)*sum(x)) / (len(x)*100) or i<((100-yuzde)*sum(x)) / (len(x)*100) )]
