05ba645d58dbb37de5ddff9ce8423ede41aa2491
ynccsyd <muhibbi_10_@hotmail.com>
2023-06-23 07:45:34 +0300

    Added Fibo Recursive

---
+++ FiboRecursive.py
@@ -0,0 +1,14 @@
+fib = {}  # Boş bir sözlük oluşturuluyor
+
+def fib2(n):
+    if n <= 1:
+        fib[str(n)] = 1
+        return 1
+    elif str(n-1) in fib:
+        fib[str(n)] = fib[str(n-1)] + fib[str(n-2)]
+        return fib[str(n)]
+    else:
+        fib[str(n)] = fib2(n-1) + fib2(n-2)
+        return fib[str(n)]
+
+print(fib2(7))
